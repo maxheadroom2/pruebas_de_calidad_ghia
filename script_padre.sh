7f3330b0076ed1d2ba45aecae6b5a3ef76059414
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-10 16:31:45 -0500

    Anexo primeros parametros asi como las variables y su seccion

--- script_padre.sh
+++ script_padre.sh
@@ -13,3 +13,30 @@
 
 # Inicio de scritp ############################################################
 
+notify-send "Prueba" "datos"
+
+#archivos de inicio
+# Estos comandos realizan los primeros archivos de extraccion de datos, asi es mas facil recuperar dicha información
+
+echo Neoteo456@ | sudo -S ls /root && sudo lshw -html > /home/maxheadroom/Test_resultados/archivos_raw/lshw.html
+echo Neoteo456@ | sudo -S ls /root && sudo lshw > /home/maxheadroom/Test_resultados/archivos_raw/lshw.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t processor > /home/maxheadroom/Test_resultados/archivos_raw/procesador.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t bios > /home/maxheadroom/Test_resultados/archivos_raw/bios.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t baseboard > /home/maxheadroom/Test_resultados/archivos_raw/placa_madre.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t memory > /home/maxheadroom/Test_resultados/archivos_raw/memoria_ram.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t processor > /home/maxheadroom/Test_resultados/archivos_raw/procesador.txt
+echo Neoteo456@ | sudo -S ls /root && sudo dmidecode -t system > /home/maxheadroom/Test_resultados/archivos_raw/sistema_general.txt
+#Falta anexar el del disco duro
+
+
+# variables
+
+ SerieComputo=$(sudo lshw.html cat *.html | grep serie | grep -Eo '[0-9]{6}' | head -1)
+ VersionBios=$()
+ MemoriaRam=$(sudo dmidecode -t memory | grep "Size" && sudo dmidecode -t memory | grep "Bank Locator" && sudo dmidecode -t memory | grep "Serial Number")
+ DiscoDuro=$()
+ Procesador=$(sudo dmidecode -t processor | grep "Version")
+ Tecnico=$()
+
+
+
