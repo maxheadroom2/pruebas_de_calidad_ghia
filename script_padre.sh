1e1b4b8714c4e09fc6ecc871624087ef16bd9239
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-10 15:55:57 -0500

    creación del script

---
+++ script_padre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+# Author: Victor Ruben Farias Rolón
+# creado 26 febrero 2016
+# modificado el 10 de Agosto 2021
+# rev-6
+
+###############################################################################
+# Descripcion: Scritp padre el cual desarrollara las pruebas del area de calidad                          
+###############################################################################
+
+
+# Inicio de scritp #############################################################
+
