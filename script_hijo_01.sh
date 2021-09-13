022d6cd8fa8182ae6a251911b3ccf47ce73e320e
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-13 13:10:08 -0500

    anexo nueva linea de dmidecode - chassis

--- script_hijo_01.sh
+++ script_hijo_01.sh
@@ -24,11 +24,19 @@ notify-send -i half-life-alyx "inicio de pruebas automaticas" "Por favor no desc
 # variables
  RutaLecturaGen=/home/maxheadroom/Test_resultados/archivos_raw/
  SerieComputo=$(cat $RutaLecturaGen/lshw.html | grep serie | grep -Eo '[0-9]{6}' | head -1)
+ ModeloPc=$(cat $RutaLecturaGen/lshw.txt | grep -Eo PCGHIA-'[0-9]{4}' | head -1)
  VersionBios=$()
- MemoriaRam=$(sudo dmidecode -t memory | grep "Size" && sudo dmidecode -t memory | grep "Bank Locator" && sudo dmidecode -t memory | grep "Serial Number")
+ MemoriaRam=$(cat $RutaLecturaGen/memoria_ram.txt | grep "Size" )
  DiscoDuro=$()
  Procesador=$(sudo dmidecode -t processor | grep "Version")
  Tecnico=$()
  Numero_CoresCPU=$(grep -m 1 'siblings' /proc/cpuinfo | grep -Eo [0-9]) #siblings son el numero de cores virtuales
 
  echo $SerieComputo
+
+ # Dialog 1
+
+dialog --begin 10 30 --backtitle "Información y Resultados" \
+--title "Este equipo de computo" \
+--msgbox "Serie $SerieComputo "$'\n'"Modelo $ModeloPc"$'\n'"Memoria RAM"$'\n'" $MemoriaRam" 20 30 ;  clear 
+
