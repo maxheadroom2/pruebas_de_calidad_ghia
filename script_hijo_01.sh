df17596681c0180354e72310e21543b94daa07fe
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-13 10:04:52 -0500

    se crea nuevo archivo script para la lectura de las variables

---
+++ script_hijo_01.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+# Author: Victor Ruben Farias Rolón
+# creado 26 febrero 2016
+# modificado el 10 de Agosto 2021
+# rev-6
+
+###############################################################################
+# Descripcion: Scritp padre el cual desarrollara las pruebas del area de calidad                          
+###############################################################################
+
+# notas 
+# git-force-clone -b main https://github.com/maxheadroom2/pruebas_de_calidad_ghia.git ./bash_scripts && sudo chmod u+x *.sh
+
+
+# Inicio de scritp ############################################################
+
+notify-send -i half-life-alyx "inicio de pruebas automaticas" "Por favor no desconecte el equipo o mueva el mouse, al finalizar las pruebas se le mandara un mensaje"
+
+#archivos de inicio
+# Estos comandos realizan los primeros archivos de extraccion de datos, asi es mas facil recuperar dicha información
+
+# variables
+
+ #ooSerieComputo=$(cat lshw.html | grep serie | grep -Eo '[0-9]{6}' | head -1)
+ VersionBios=$()
+ MemoriaRam=$(sudo dmidecode -t memory | grep "Size" && sudo dmidecode -t memory | grep "Bank Locator" && sudo dmidecode -t memory | grep "Serial Number")
+ DiscoDuro=$()
+ Procesador=$(sudo dmidecode -t processor | grep "Version")
+ Tecnico=$()
+ Numero_CoresCPU=$(grep -m 1 'siblings' /proc/cpuinfo | grep -Eo [0-9]) #siblings son el numero de cores virtuales
