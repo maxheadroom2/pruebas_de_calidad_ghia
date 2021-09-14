50d25d0534597d5b5d0ab443901b6462bf19a5d4
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-14 10:57:24 -0500

    ajusto variable que habia borrado

--- script_hijo_01.sh
+++ script_hijo_01.sh
@@ -58,6 +58,8 @@ done
 
  # Dialog 1
 
+TenicoMatch=(cat $RutaLecturaGen/temp_tec.txt) # lee los datos del tecnico
+
 dialog --begin 10 30 --backtitle "Información y Resultados" \
 --title "Este equipo de computo lo realizo " \
 --msgbox "Tecnico:"$'\n'"$TenicoMatch"$'\n'"Serie $SerieComputo "$'\n'"Modelo $ModeloPc"$'\n'"Memoria RAM:"$'\n'" $MemoriaRam"$'\n'"Tarjeta Madre Modelo:"$'\n'" $Placa_Madre"$'\n'"Tarjeta Madre Version:"$'\n'" $Placa_Madre_Ver"$'\n'"Procesador:"$'\n'" $Procesador"$'\n'"Bios versión:"$'\n'" $VersionBios"$'\n' 30 70 ;  clear
