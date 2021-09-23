78b95852b172deda9bd51d8bba8a4d51d192cd7b
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-23 13:23:10 -0500

    prueba de array en dialog

--- script_hijo_01.sh
+++ script_hijo_01.sh
@@ -124,10 +124,25 @@ funcion_dialog_resultado(){
     Msj_7="Datos de Disco(s) instalados ↓"
     Msj_8="Datos de Slots de Memoria RAM ↓"
        sleep .5s;
+        array_msgbox=(
+        "$Msj_0""\Zu$D0\Zu"$'\n'
+        "$Msj_1""$D1"$'\n'
+        "$Msj_2""$D2"$'\n'
+        "$Msj_3""$D3"$'\n'
+        "$Msj_4""$D4"$'\n'
+        "$Msj_5""$D5"$'\n'
+        "$Msj_6""$D6"$'\n'
+        "$Msj_7"$'\n'
+        "$D7"$'\n'
+        "$Msj_8"$'\n'
+        "$D8"$'\n' 26 90
+
+    ) 
     TenicoMatch=$(cat $Ru/temp_tec.txt)# lee los datos del tecnico
     dialog --colors --begin 5 5 --backtitle "Información y Resultados"  \
     --title "Resultados de lectura del Equipo de computo " \
-    --msgbox "$Msj_0""\Zu$D0"$'\n'"$Msj_1""$D1"$'\n'"$Msj_2""$D2"$'\n'"$Msj_3""$D3"$'\n'"$Msj_4""$D4"$'\n'"$Msj_5""$D5"$'\n'"$Msj_6""$D6"$'\n'"$Msj_7"$'\n'"$D7"$'\n'"$Msj_8"$'\n'"$D8"$'\n' 26 90 ;  clear
+    --msgbox "${array_msgbox[@]}" && unset array_msgbox ;  clear
+    
 }
 
 funcion_xterm_curseofwar(){
