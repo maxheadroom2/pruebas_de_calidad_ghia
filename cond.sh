cef5f7d9187084b3bdbb323abd41063bff1f8101
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-27 18:55:09 -0500

    ajustes

--- cond.sh
+++ cond.sh
@@ -4,9 +4,8 @@ read -p "Enter two numbers a and b : " a b
 #cd /home/maxheadroom/Scripts/bash_script/ ;
 ./sum.sh 
 
-1=$a
-2=$b
-sum 1 2   
+
+sum $a $b   
 # Now the above line is high octane part
 # The "./" mentions that the "sum" script is in the same  folder as the driver
 # The values we populated went in as parameters
