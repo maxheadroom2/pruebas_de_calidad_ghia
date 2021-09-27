84a9db15aaa6774c0444c4b5359d60a8930e6af5
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-27 17:53:05 -0500

    cambios

--- cond.sh
+++ cond.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter two numbers a and b : " a b
 # Well you should do sanitize the user inputs for legal values
-cd /home/maxheadroom/Scripts/bash_script/
+#cd /home/maxheadroom/Scripts/bash_script/ ;
 ./sum.sh "$a" "$b"
 # Now the above line is high octane part
 # The "./" mentions that the "sum" script is in the same  folder as the driver
