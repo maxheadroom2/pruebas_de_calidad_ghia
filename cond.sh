f8e1e96200acf03509439cd00b1fb19361883797
maxheadroom2 <sinteticbeat@hotmail.com>
2021-09-27 17:40:59 -0500

    pruebas

---
+++ cond.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter two numbers a and b : " a b
+# Well you should do sanitize the user inputs for legal values
+./sum "$a" "$b"
+# Now the above line is high octane part
+# The "./" mentions that the "sum" script is in the same  folder as the driver
+# The values we populated went in as parameters
+# Mind the double quotes.If the actual input has spaces double quotes serves 
+# to preserve theme
+# We are all done for the driver part
---
+++ sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# The script acts as a wrapper for the function sum
+# Here we calculate the sum of parameters passed to the script
+function sum(){
+(( sum = $1 + $2 ))
+echo "Sum : $sum"
+}
+sum "$@" # Passing value from the shell script wrapper to the function
+# $@ expands to the whole parameter input.
+# What is special about "$@". Again, the spaces in the original parameters are preserved if those parameters were of the form "`A B"`.
